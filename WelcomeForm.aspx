d9074d15605d6d1ebef51e2bcbe63bc1f520b68a
dineshrbtek <dinesh.kumar@rbtekspace.com>
2017-06-08 15:15:37 +0530

    Added my LoginForm
    Here i created Login Form.

---
+++ WelcomeForm.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WelcomeForm.aspx.cs" Inherits="CreatWebForm.WelcomeForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <h2 style="text-align:center; background-color:blue;color:white">WELCOME TO MY LOGIN FORM</h2>
+    </div>
+        <div>
+            <table style="margin-left:40%">
+                <tr>
+                  <th>Enter User Name</th>
+                    <td><asp:TextBox ID="txtusername" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <th>Enter Password</th>
+                    <td><asp:TextBox ID="txtpassword" runat="server" TextMode="Password"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <th></th>
+                    <td><asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click"/>
+                        &ensp;
+                        <input type="reset" value="Cancel" /><br />
+                        <asp:Label ID="lblstatus" runat="server"></asp:Label>
+                    </td>
+                </tr>
+                   
+
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WelcomeForm.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CreatWebForm
+{
+    public partial class WelcomeForm : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString);
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand("select * from Registration where UserName=@UserName and Password=@Password", con);
+            cmd.Parameters.AddWithValue("@UserName", txtusername.Text);
+            cmd.Parameters.AddWithValue("@Password", txtpassword.Text);
+            sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            con.Open();
+            sda.Fill(dt);
+            int i = cmd.ExecuteNonQuery();
+            if(dt.Rows.Count>0)
+            {
+                lblstatus.Text = "<b style='color:green;'>Login Successfully!!</b>";
+                Response.Redirect("MainForm.aspx");
+            }
+            else
+            {
+                lblstatus.Text = "<b style='color:red;'>Login Faild</b>";
+                con.Close();
+            }
+        }
+    }
+}
